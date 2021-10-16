bd62eba9d8637ee1ee1a25a85c4f60b3fd3d3243
Dylan Castle <ghostjammerc@gmail.com>
2021-10-15 19:32:34 -0700

    10/15/21 NOT FINISHED -- Started looking into using OpenCv with the camera for autonomous detection. A pipeline system (NerdsPipeline) was started, but the processing method needs work. A test class (CameraTest) was created to see how the camera could work.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Scalar;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@TeleOp
+@SuppressWarnings({"unused"})
+public class CameraTest extends OpMode
+{
+    static final int CAMERA_WIDTH = 640;
+    static final int CAMERA_HEIGHT = 360;
+
+    NerdsPipeline detector = new NerdsPipeline();
+    OpenCvCamera camera;
+
+    //bounds for colors            Y      Cr     Cb       (keep Y)
+    Scalar lowerBound = new Scalar(000.0, 128.0, 000.0);
+    Scalar upperBound = new Scalar(255.0, 160.0, 096.0);
+
+    @Override
+    public void init()
+    {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        camera.setPipeline(detector);
+        camera.openCameraDevice();
+        camera.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+    }
+
+    @Override
+    public void loop()
+    {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NerdsPipeline.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class NerdsPipeline extends OpenCvPipeline
+{
+    /*
+     * 1 = scoring level 1
+     * 2 = scoring level 2
+     * 3 = scoring level 3
+     */
+    private int logicPos = 0;
+
+    @Override
+    public Mat processFrame(Mat input)
+    {
+        //create an organic version of the Mat
+        Mat preview = new Mat();
+        Imgproc.cvtColor(input, preview, Imgproc.COLOR_RGB2HSV);
+
+        //create HSV values for the low and high end of what is considered to be yellow
+        Scalar lowHSV = new Scalar(20, 100, 100);
+        Scalar highHSV = new Scalar(30, 255, 255);
+    }
+
+    public int logicPos()
+    {
+        return logicPos;
+    }
+}
