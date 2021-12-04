69958f0f7fa9c21a701050dfe232a5038ecf31a0
Dylan Castle <ghostjammerc@gmail.com>
2021-12-03 19:31:32 -0800

    12/03/21 Bob - Added servo arms to sift through cargo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bob/Bob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bob/Bob.java
@@ -4,12 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 @SuppressWarnings({"unused"})
 public class Bob extends OpMode
 {
     private DcMotor driveRF, driveRB, driveLF, driveLB;
+    private Servo leftArm, rightArm;
 
     @Override
     public void init()
@@ -18,11 +20,15 @@ public class Bob extends OpMode
         driveRB = hardwareMap.get(DcMotor.class, "driveRB");
         driveLF = hardwareMap.get(DcMotor.class, "driveLF");
         driveLB = hardwareMap.get(DcMotor.class, "driveLB");
+        leftArm= hardwareMap.get(Servo.class, "leftArm");
+        rightArm = hardwareMap.get(Servo.class, "rightArm");
 
         driveRF.setDirection(DcMotor.Direction.FORWARD);
         driveRB.setDirection(DcMotor.Direction.FORWARD);
         driveLF.setDirection(DcMotor.Direction.REVERSE);
         driveLB.setDirection(DcMotor.Direction.REVERSE);
+        leftArm.setDirection(Servo.Direction.REVERSE);
+        rightArm.setDirection(Servo.Direction.FORWARD);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -30,13 +36,20 @@ public class Bob extends OpMode
 
     public void loop()
     {
-        double xPow = gamepad1.left_stick_x;
-        double yPow = gamepad1.left_stick_y;
-        double rxPow = gamepad1.right_stick_x;
+        //power values for drive train
+        double xPow, yPow, rxPow;
+        xPow = gamepad1.left_stick_x;
+        yPow = gamepad1.left_stick_y;
+        rxPow = gamepad1.right_stick_x;
 
+        //give power to drive train (mecanum wheel movement)
         driveRF.setPower(yPow - xPow -rxPow);
         driveRB.setPower(yPow + xPow - rxPow);
         driveLF.setPower(yPow + xPow + rxPow);
         driveLB.setPower(yPow - xPow + rxPow);
+
+        //maneuver front arms to sift through cargo
+        leftArm.setPosition(gamepad1.left_trigger/2);
+        rightArm.setPosition(gamepad1.right_trigger/2);
     }
 }
